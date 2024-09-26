0cee55eb9f2b1f4be72b3e3b4044d884e32f120b
natruyd123 <ralphyap23@gmail.com>
2024-09-26 17:34:51 +0800

    Del config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-# config.py
-def dbconfig(app):
-    app.config['MYSQL_HOST'] = 'localhost'
-    app.config['MYSQL_USER'] = 'root'
-    app.config['MYSQL_PASSWORD'] = ''
-    app.config['MYSQL_DB'] = 'cybersimple'
